32ffb41c35b3156b00ba109bcf9a197df285ca2f
Atomic Antimatter <atomicantimatter@gmail.com>
2012-04-26 14:27:10 -0400

    fixed bullet spawn spot

--- Tanks/Bullets/Bullet.java
+++ Tanks/Bullets/Bullet.java
@@ -34,8 +34,8 @@ public abstract class Bullet
         vx = _v*Math.cos(ba) - (parent.getSpeed()*Math.cos(parent.getDirection()));
         vy = _v*Math.sin(ba) - (parent.getSpeed()*Math.sin(parent.getDirection()));
         h = _h;
-        x -= form().getBounds().width/2*Math.cos(ba);
-        y -= form().getBounds().height/2*Math.sin(ba);
+        x -= form().getBounds().width/2*Math.cos(ba+Math.PI/2);
+        y -= form().getBounds().height/2*Math.sin(ba+Math.PI/2);
     }
     
     public synchronized void move() 
